979990768a1e76c555de91456dc86337be9bc085
Alysson Salgado <alysson.salgado@gauge.com.br>
2020-02-07 15:41:54 -0300

    Ajustes

--- src/views/userView/ModalUser.js
+++ src/views/userView/ModalUser.js
@@ -4,6 +4,7 @@ import { Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap'
 function ModalComponent(props) {
     const [isOpen, setIsOpen] = useState(false)
     const [user, setUser] = useState("")
+    const [passConfirm, setPassConfirm] = useState("")
     
   useEffect(() => {
     setIsOpen(props.isOpen);
@@ -22,8 +23,13 @@ function ModalComponent(props) {
   }
 
   function saveModal(){ 
-    props.onSave(user)
-    closeModal()
+    if(user.pass != passConfirm){
+      window.alert("Senha não confere");
+    }
+    else{
+      props.onSave(user)
+      closeModal()
+    }
   }
 
   return (
@@ -35,22 +41,32 @@ function ModalComponent(props) {
                     {props.title}
                 </ModalHeader>
                 
-                <ModalBody>
-                  <div className="form-group">
+                <ModalBody className="row">
+                  <div className="form-group col-12">
                     <label for='userName'>Name</label>
                     <input type='text' className="form-control" id='userName' placeholder='User name'
                       onChange={event => setUser({...user, name:event.target.value})} value={user.name}/>
                   </div>   
-                  <div className="form-group">
+                  <div className="form-group col-12">
                     <label for='userLogin'>Login</label>
                     <input type='text' className="form-control" id='userLogin' placeholder='User login'
                       onChange={event => setUser({...user, login:event.target.value})} value={user.login}/>
                   </div>   
-                  <div className="form-group">
+                  <div className="form-group col-6">
                     <label for='userPass'>Password</label>
                     <input type='password' className="form-control" id='userPass' placeholder='User password'
                       onChange={event => setUser({...user, pass:event.target.value})} value={user.pass}/>
                   </div>   
+                  <div className="form-group col-6">
+                    <label for='userPass'>Repet password</label>
+                    <input type='password' className="form-control" id='userPass' placeholder='User password'
+                      onChange={event => setPassConfirm(event.target.value)} value={passConfirm}/>
+                  </div>   
+                  <div class="form-check ml-3 col-10">
+                    <input type="checkbox" class="form-check-input" id="checkboxIsAdmin"
+                      onChange={event => setUser({...user, admin:event.target.value})} value={user.admin}/>
+                    <label class="form-check-label" for="checkboxIsAdmin">Admin</label>
+                  </div>
                 </ModalBody>
 
                 <ModalFooter>
